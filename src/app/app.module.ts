7a66d346be0b9b753c3d5405a2acad5d04a2179f
Carlos Angulo <carlosalbertoangulomendoza@gmail.com>
2018-10-19 11:44:35 -0500

    [ADD] Project scafold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,35 +4,23 @@ import { NgModule } from '@angular/core';
 //Routes
 import { APP_ROUTES } from 'src/app/app.routes';
 
+//Modules
+import { PagesModule } from 'src/app/pages/pages.module';
+
 //Components
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graphics1Component } from './pages/graphics1/graphics1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graphics1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
+    PagesModule,
     APP_ROUTES
   ],
   providers: [],
